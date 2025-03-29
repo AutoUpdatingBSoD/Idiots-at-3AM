5a295a1b7f50d70adeda681d8be2fdd1d906e62b
Michael Hammond <mhammond9@radford.edu>
2025-03-29 15:34:55 -0400

    Update search.js

---
+++ docs/search.js
@@ -0,0 +1,28 @@
+function search_tutorial() {
+    let input = document.getElementById('searchbar').value
+    input=input.toLowerCase();
+    let x = document.getElementsByClassName('tutorials');
+      
+    for (i = 0; i < x.length; i++) { 
+        if (!x[i].innerHTML.toLowerCase().includes(input)) {
+            x[i].style.display="none";
+        }
+        else {
+            x[i].style.display="list-item";                 
+        }
+    }
+}
+            
+$(document).ready(function() {
+    var csvdata = [    
+        ["Edit Ada in Visual Studio: The Right Way!","RunAdaInVSCode.html"],
+        ["How to install Cisco AnyConnect VPN on Arch Linux Distributions using the Tarball","https://www.reddit.com/r/ManjaroLinux/comments/iax5q2/howto_install_the_official_cisco_anyconnect_vpn/"],
+        ["Common Cybersecurity References","antihack.html"]
+        ];
+        let x = document.getElementsByClassName('tutorials');
+        for (i = 0; i < x.length; i++) { 
+            if (x[i].innerText == csvdata[i][0]) {
+                x[i].href = csvdata[i][1];
+            }          
+        }
+    });
